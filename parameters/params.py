1823cdf10935b9737e461bd135fcd23b1a3e98e3
Dami Adesina <damilolaadesina@Damilolas-MBP-2.attlocal.net>
2023-04-27 16:18:23 -0500

    shap added

--- parameters/params.py
+++ parameters/params.py
@@ -9,3 +9,14 @@ data_file_path = os.path.join(os.path.join(main_path, DATA_FOLDER), TITANIC_DATA
 
 
 target_feat = "Survived"
+
+# catboost_param = {'iterations':5000,
+#         'learning_rate':0.01,
+#         'cat_features':categorical_features,
+#         'depth':3,
+#         'eval_metric':'AUC',
+#         'verbose':200,
+#         'od_type':"Iter", # overfit detector
+#         'od_wait':5000, # most recent best iteration to wait before stopping
+#         'random_seed': 1
+#           }
