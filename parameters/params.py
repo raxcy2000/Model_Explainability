2836428a556db7b1d6ef4e2fc097dec74e250589
Dami Adesina <damilolaadesina@Damilolas-MBP-2.attlocal.net>
2023-04-27 22:49:55 -0500

    shap explain

--- parameters/params.py
+++ parameters/params.py
@@ -1,22 +1,16 @@
 import os
 
 TITANIC_DATASET = "titanic.csv"
+CALIFORNIA_DATASET = "california_housing_data.csv"
+BOSTON_DATASET = "Boston_housing_dataset.csv"
 
 DATA_FOLDER = "data"
 main_path = os.getcwd()
 
 data_file_path = os.path.join(os.path.join(main_path, DATA_FOLDER), TITANIC_DATASET)
+cali_data_file_path = os.path.join(os.path.join(main_path, DATA_FOLDER), CALIFORNIA_DATASET)
+boston_data_file_path = os.path.join(os.path.join(main_path, DATA_FOLDER), BOSTON_DATASET)
 
 
 target_feat = "Survived"
 
-# catboost_param = {'iterations':5000,
-#         'learning_rate':0.01,
-#         'cat_features':categorical_features,
-#         'depth':3,
-#         'eval_metric':'AUC',
-#         'verbose':200,
-#         'od_type':"Iter", # overfit detector
-#         'od_wait':5000, # most recent best iteration to wait before stopping
-#         'random_seed': 1
-#           }
